8befb7d03ab1e32ba000313e18fc66bbbd2ddc79
Naïké Dramé <naike.drame.dev@gmail.com>
2025-01-07 14:52:42 +0100

    theory methods

--- MyFirstNetApplication/Program.cs
+++ MyFirstNetApplication/Program.cs
@@ -672,76 +672,238 @@ Console.WriteLine("Bienvenue " + input + " ! ♥");
 
 
 #region Structures
-//-------------- Grosse différence entre struct & une class & un record: ----------------
-// struct =  de type valeur ---> n'est pas un élément null
-// class =  de type référence ---> peut être nullable
-// record = constante, on ne peut plus modifier les infos venant de l'extérieur = info de base fixe (// ajouter readonly)
+////-------------- Grosse différence entre struct & une class & un record: ----------------
+//// struct =  de type valeur ---> n'est pas un élément null
+//// class =  de type référence ---> peut être nullable
+//// record = constante, on ne peut plus modifier les infos venant de l'extérieur = info de base fixe (// ajouter readonly)
 
 
-PersonStruct p = new PersonStruct();
-p.age = 42;
-p.fname = "Khun";
-p.name = "Ly";
+//PersonStruct p = new PersonStruct();
+//p.age = 42;
+//p.fname = "Khun";
+//p.name = "Ly";
 
-PersonStruct p2 = p;
-Console.WriteLine(p2.age); // 42
+//PersonStruct p2 = p;
+//Console.WriteLine(p2.age); // 42
 
-PersonStruct.Test t = new();
-
-
-PersonClass p = new();
-p.age = 42;
-p.fname = "Khun";
-p.name = "Ly";
-PersonClass p2 = p;
-p2.age = 43;
-Console.WriteLine(p.age); // 43
-
-PersonClass p3 = new();
-p3.age = 42;
-p3.fname = "Mike";
-p3.name = "Person";
+//PersonStruct.Test t = new();
 
 
-PersonRecord record1 = new PersonRecord("Ly", "Khun", 42);
-// record1.name = "test"; // error : on ne peut pas modifier
-Console.WriteLine(record.age);
+//PersonClass p = new();
+//p.age = 42;
+//p.fname = "Khun";
+//p.name = "Ly";
+//PersonClass p2 = p;
+//p2.age = 43;
+//Console.WriteLine(p.age); // 43
+
+//PersonClass p3 = new();
+//p3.age = 42;
+//p3.fname = "Mike";
+//p3.name = "Person";
 
 
-// Création d'un struct = création d'un type
-// on peut créer des struct imbriquées (slide 167) 
-struct PersonStruct
+//PersonRecord record1 = new PersonRecord("Ly", "Khun", 42);
+//// record1.name = "test"; // error : on ne peut pas modifier
+//Console.WriteLine(record.age);
+
+
+//// Création d'un struct = création d'un type
+//// on peut créer des struct imbriquées (slide 167) 
+//struct PersonStruct
+//{
+//    // !!! on ne peut pas définir de valeur par défaut sauf si déclarée avec const ou static
+//    public string name; // ""
+//    public string fname; // ""
+//    public int age; //0
+//    const int test = 42;
+
+//    //peut avoir des méthodes
+//    public void Speak() { }
+
+//    //peut avoir des struct internes
+//    public struct Test
+//    {
+
+//    }
+
+
+//}
+
+
+//class PersonClass
+//{
+//    public string name;
+//    public string fname;
+//    public int age;
+//}
+
+//record PersonRecord(string name, string fname, int age)
+//{
+//    public void Speak() { }
+//}
+
+
+#endregion
+
+
+#region Méthodes 
+//Fonction = ensemble d'instructions encapsulées dans un espace mémoire, avec un nom et le type de retour de ladite fonction
+
+// appel de la fonction Speak()
+Person p;
+p.Speak();
+p.Speak();
+p.Speak();
+p.Speak();
+p.Speak();
+
+// appel de la fonction Test()
+Test();
+Test();
+Test();
+Test();
+Test();
+Test();
+
+// appel de la fonction FactorielDeNb() qui retourne n!
+int value = FactorielDeNb(5);
+Console.WriteLine(value);
+
+// définition de la fonction Test() qui ne retourne rien
+void Test()
 {
-    // !!! on ne peut pas définir de valeur par défaut sauf si déclarée avec const ou static
-    public string name; // ""
-    public string fname; // ""
-    public int age; //0
-    const int test = 42;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Ceci est un test");
+    Console.ResetColor();
+}
 
-    //peut avoir des méthodes
-    public void Speak() { }
-
-    //peut avoir des struct internes
-    public struct Test
+// définition de la fonction FactorielDe5() qui retourne un int
+int FactorielDeNb(int nb)
+{
+    int result = 1;
+    for (int i = 2; i <= nb; i++)
     {
+        result *= i;
+    }
+    return result;
+}
 
+Console.WriteLine(Divide(1, 3));
+Console.WriteLine(Divide(42)); //on remplace le param a
+Console.WriteLine(Divide(b : 42)); // on remplace le param b
+
+double Divide( double a = 1, double b = 1) // avec params par défaut
+{
+    return a / b;
+}
+
+// un param précdé du mot-clé in n'est plus modifiable au sein de la fonction
+void Test44(in string s = "", int nb = 55)
+{
+    // on ne peut plus le modifier
+    //s = "fjhjfhfskj";
+}
+
+double Sum(params double[] values)   //lLorsque nous ignorons le nombre de paramètres à passer, on peut passer un ensemble de params ou un array de doubles
+{
+    double result = 0;
+
+    foreach (var item in values)
+    {
+        result += item;
     }
 
-
+    return result;
 }
 
+Console.WriteLine("--------------");
+Console.WriteLine(Sum(42, 7, 8, 9, 11, 33, 99));
+// ||     ||       ||
+Console.WriteLine(Sum([42, 7, 8, 9, 11, 33, 99]));
 
-class PersonClass
+
+int v = 42;
+void UpdateValue(int v2)
 {
-    public string name;
-    public string fname;
-    public int age;
+    v2 = 45;
 }
 
-record PersonRecord(string name, string fname, int age)
+UpdateValue(v);
+Console.WriteLine(v); // 42 car on modifie v2 et non v
+
+
+//ref : on passe en param l'adresse mémoire de l'élément passé en param, et on peut ensuite modifier sa valeur
+void UpdateValueWithRef(ref int v2)
 {
-    public void Speak() { }
+    v2 = 45;
 }
 
+UpdateValueWithRef(ref v);
+Console.WriteLine(v); // 45
 
+
+List<int> list = [1,2,3];  
+List<int> list2 = [1,2,3];   
+
+UpdateList(list);
+Console.WriteLine(list.Last());  // 4
+
+void UpdateList(List<int> l)
+{
+    l.Add(4);
+}
+
+void UpdateListWithRef(ref List<int> l)
+{
+    l = [..l, 4];
+}
+
+UpdateListWithRef(ref list2);
+Console.WriteLine(list2.Last()); // 4 car on modifie la ref list2
+
+// --- ou == ref
+void UpdateValueWithRef2(out int v2)
+{
+    v2 = 45;
+}
+
+UpdateValueWithRef2(out v);
+Console.WriteLine(v); // 45
+
+
+// Surcharge de méthode : un seul nom de méthode, mais en fonction du param appelé, la bonne méthode sera appelée
+/*
+public void MyMethod()
+{
+    //
+}
+public void MyMethod(int i)
+{
+    //
+}
+public void MyMethod(string s)
+{
+    //
+}
+
+*/
+
+
+//---------------//
+
+struct Person
+{
+    // définition de la fonction ----> ne pas oublier d documenter la fonction avec un summary, plus qu'avec des commentaires
+    /// <summary>
+    /// Ecrit dans la console qqch
+    /// </summary>
+    public void Speak()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Bonjour, je m'appelle Khun");
+        Console.ResetColor();
+    }
+
+}
 #endregion
